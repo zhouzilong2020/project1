e45790ac13949952ccc0a684d8320c3b3569c6ec
周子龙 <zhouzilong@promote.cache-dns.local>
2020-03-08 16:46:10 +0800

    homepage&index&loginpage finish

--- Flask测试/app.py
+++ Flask测试/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 from models import *
@@ -8,6 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:20001003@localhos
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"   #filesystem允许用户对数据进行更改、插入、删除等操作
+app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
 
 db.init_app(app)
 
@@ -17,9 +18,48 @@ db.init_app(app)
 def index():
     return render_template('index.html')
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        user = User(request.form.get('user_id'), request.form.get('password'))
+        print(user.user_id, user.password)
+        #如果成功登录
+        if user.login():
+            return redirect(url_for('homepage'), user=user)
+        else:
+            return redirect(url_for('index'))
+
     return render_template('login.html')
+
+@app.route('/homepage', methods = ['GET', 'POST'])
+def homepage():
+    user = User('meizijun' ,12)
+    if request.method == 'POST':
+        book = Book(request.form.get('title'),
+                    request.form.get('isbn'),
+                    request.form.get('author'),
+                    request.form.get('year'))
+        if book.search():
+            1+1
+        else:
+            return redirect(url_for('error_book_no_found'))
+
+    return render_template('homepage.html', user=user)
+
+
+
+
+
+
+
+
+
+
+
 
 def createTable():
     with app.app_context():
--- Flask测试/models.py
+++ Flask测试/models.py
@@ -18,32 +18,34 @@ class User(db.Model):
         self.password = password
         self.validity = 0
 
+    def __repr__(self):
+        return '<user %s>' % self.user_id
+
     def currentStatus(self):
         return True if validity == 1 else False
-        
+
 
     #log in user
-    def logIn(self):
+    def login(self):
         #判断当前用户是否存在  TODO::注意安全性检查！
-        exist_user_password = db.session.execute(f"SELECT password .\
-                                          FROM users .\
-                                          WHERE user_id = {self.user_id}").first()
+        try:
+            exist_user_password = db.session.execute(f'''SELECT password FROM users WHERE user_id = "{self.user_id}" ''').first()
         #当前用户名存在，并且密码匹配
-        if exist_user_password is not None: 
-            if exist_user_password == self.password:
-                validity = 1
-                print("login successfully!")
-        else:
-            print("does not exist such id or passworf faills to match")
+            if exist_user_password is not None:
+                if exist_user_password == self.password:
+                    validity = 1
+                    return True
+        except:
+            return False
 
     #log out user
-    def logOut(self):
+    def logout(self):
         if validity == 1:
             validity = 0
             print("you have logged out")
         else:
             print("error!")
-        
+
     #add a new user
     def addUser(self):
         is_exist = db.session.excute(f"SELECT count(*) .\
@@ -81,10 +83,11 @@ class Book(db.Model):
     author = db.Column(db.String, nullable = False)
     year = db.Column(db.Integer, nullable = False)
 
-    def __init__(self, isbn, auther, title):
+    def __init__(self, isbn, auther, title, year):
         self.isbn = isbn
         self.auther = auther
         self.title = title
+        self.year = year
 
     #insert a book to database
     def insert(self):
@@ -92,18 +95,21 @@ class Book(db.Model):
         self.id = Book.id
         db.session.excute("") #TODO
 
-    #search a book through isbn/auther/title with partialy or entirely information 
+    #search a book through isbn/auther/title with partialy or entirely information
     def search(self):
-        isbnExist = db.session.excute(f"SELECT * .\
-                                        FROM books .\
-                                        WHERE books.isbn LIKE ''%{self.isbn}%'' ").fetchall()
-        titleExist = db.session.excute(f"SELECT * .\
-                                         FROM books .\
-                                         WHERE books.title LIKE ''%{self.title}%'' ").fetchall()
-        AuthorExist = db.session.excute(f"SELECT * .\
-                                          FROM books .\
-                                          WHERE books.title LIKE ''%{self.author}%'' ").fetchall()
-        return [isbnExist, titleExist, AuthorExist]
+        try:
+            isbnExist = db.session.excute(f"SELECT * \
+                                            FROM books \
+                                            WHERE books.isbn LIKE ''%{self.isbn}%'' ").fetchall()
+            titleExist = db.session.excute(f"SELECT * \
+                                             FROM books \
+                                             WHERE books.title LIKE ''%{self.title}%'' ").fetchall()
+            AuthorExist = db.session.excute(f"SELECT * \
+                                              FROM books \
+                                              WHERE books.title LIKE ''%{self.author}%'' ").fetchall()
+            return [isbnExist, titleExist, AuthorExist]
+        except:
+            return False
 
 
 class Review(db.Model):
@@ -117,16 +123,16 @@ class Review(db.Model):
     #与books的联系
     isbn = db.Column(db.String, db.ForeignKey("books.isbn"), nullable = False)
     #反向联系
-    book = relationship("Book", back_populates="reviews")
+    book = relationship("Book", backref=db.backref('reviews', lazy='dynamic'))
 
     #与users的联系
     user_id = db.Column(db.String, db.ForeignKey("users.user_id"), nullable = False)
     #反向联系
-    users = relationship("User", back_populates="reviews")
+    users = relationship("User", backref=db.backref('reviews', lazy='dynamic'))
 
     comment = db.Column(db.String, nullable = False)
     rate = db.Column(db.Integer, nullable = False)
-    
+
     def writeReview(self, comment, title, rate, revier_author):
         count +=1
         self.id = Review.id
@@ -134,8 +140,3 @@ class Review(db.Model):
                            (id, title, user_id, comment, rate) .\
                            VALUES ('{self.id}','{self.title}', '{self.user_id}', '{self.comment}', '{self.rate}' ")
         print("comment success!")
-        
-
-
-
-        
